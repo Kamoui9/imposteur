5bc367fa03b400fd3f2c829bb9ae8902f9d581eb
Kamoui9 <pannetier.mateo@gmail.com>
2023-03-10 19:28:18 +0100

    on avance lentement et c'est tout

--- website/index.php
+++ website/index.php
@@ -10,14 +10,21 @@
 </head>
 <body>
     
-    <?php echo("Ceci est du texte"); ?>
-
     <h2>wanna play ?</h2>
-    <input type="text" placeholder="game name ?" id="game-name">
+    <input type="text" placeholder="enter your username" id="game-name">
     <input type="button" id="create-game-button" value="CREATE GAME" title="min 3 caracteres" disabled>
     <input type="button" value="JOIN GAME" disabled>
+
+    <?php 
+
+
+    echo $_GET['nom']
+    
+    
+    ?>
 
 </body>
 
 <script src="script.js"></script>
+
 </html>
